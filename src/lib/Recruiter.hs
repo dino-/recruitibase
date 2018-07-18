8ad70a89fffb5b1fd7d80645b561df974fff8f95
Dino Morelli <dino@ui3.info>
2018-07-18 14:30:57 -0400

    Many clean-up changes
    - Now parsing the notes field into a (Map Text Text) so we can use the
      note date as a lookup key.
    - Combined addr1 and csz into one address field
    - Renamed some other fields:
      - firstName -> givenName
      - co -> company
      - addr1 -> address

--- src/lib/Recruiter.hs
+++ src/lib/Recruiter.hs
@@ -1,31 +1,44 @@
 module Recruiter
   ( Recruiter (..)
   , decodeEither'
+
+  -- re-exporting
+  , defConfig
+  , encodePretty
   ) where
 
 import Data.Aeson
 import Data.ByteString
+import Data.Map
 import Data.Text
 import qualified Data.Yaml as Y
+import Data.Yaml.Pretty
 import GHC.Generics
 
 
+newtype Note = Note (Map Text Text)
+  deriving (Generic, Show)
+
+instance FromJSON Note
+instance ToJSON Note
+
+
 data Recruiter = Recruiter
-  { firstName :: Text
+  { givenName :: Text
   , surname :: Maybe Text
-  , co :: Maybe Text
-  , addr1 :: Maybe Text
-  , csz :: Maybe Text
+  , company :: Maybe Text
+  , address :: Maybe Text
   , phone :: Maybe Text
   , email :: Maybe Text
   , web :: Maybe Text
   , kind :: Maybe Text
   , active :: Bool
-  , notes :: Text
+  , notes :: [Note]
   }
   deriving (Generic, Show)
 
 instance FromJSON Recruiter
+instance ToJSON Recruiter
 
 
 decodeEither' :: ByteString -> Either Y.ParseException [Recruiter]
--- src/recruitibase/Main.hs
+++ src/recruitibase/Main.hs
@@ -1,10 +1,16 @@
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C8
 
 import Recruiter
 
 
 main :: IO ()
 main = do
+  -- Loading data in
   contents <- BS.readFile "/home/dino/doc/jobs/recruiters.yaml"
   let result = decodeEither' contents
-  print result
+  --print result
+
+  -- Writing it back out
+  let recoded = encodePretty defConfig <$> result
+  either print C8.putStrLn recoded
